6fe72279a0c3279403e6140b43cc8298a6bb6480
Angadpal Singh Hundal <anh855@usask.ca>
2022-10-20 19:04:24 -0600

    Successful server run -Angad

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+var path = require("path")
 
 const app = express()
-const port = 3000
+const port = 3100
+
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  
+    res.render('home.html')
 })
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

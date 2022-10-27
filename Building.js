475c05bef55b1fea1f7dc08ebe919d2d5f64a647
Zach Uchacz <zau937@mail.usask.ca>
2022-10-26 18:41:43 -0600

    Created Building class with constructor

---
+++ Building.js
@@ -0,0 +1,10 @@
+import Searchable from ".\Searchable.js";
+
+class Building extends Searchable {
+    // class constructor takes the name of the building
+    constructor(name) {
+        super(name);
+        this.name = name;
+    }
+    
+}

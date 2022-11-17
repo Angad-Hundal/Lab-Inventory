78da734a1ceb23c987875942bb4b22e443418455
Angadpal Singh Hundal <anh855@usask.ca>
2022-11-17 14:16:29 -0600

    Navigation enabled and blueprint for signoff

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ var path = require("path");
 const fs = require('fs')
 
 
-const PORT = 3670
+const PORT = 3697
 const app = express()
-const HOST = '10.0.0.145';
+const HOST = '174.2.74.56';
+//const HOST = '10.0.0.145';
 
 
 
@@ -20,16 +21,17 @@ app.use(bodyParser.json());
 const pool = mySql.createPool({
   connectionLimit: 100,
   database: "mydb",
-  host: "10.0.0.145",
+  host: "174.2.74.56",
+  // host: "10.0.0.145",
   user: "root",
-  password: "12345"
+  password: "cmpt370pass"
 })
 
 
 
 // display items page
 app.get('/', (req, res) => {
-  res.redirect("/search.html");
+  res.redirect("/all.html");
 })
 
 
@@ -62,6 +64,45 @@ app.post('/items', (req, res) => {
     console.log("CONNECTION ESTABLISHED")
 
     connection.query('INSERT INTO inventory SET ?', dataForm, (err, rows) => {
+      connection.release();
+
+      if (err) {
+        console.log(err);
+      }
+    })
+  })
+})
+
+
+
+
+
+
+
+// signoff.html
+// update mysql for used value
+app.post('/signoff', (req, res) => {
+
+  const dataForm = req.body;
+
+  var id = req.body.id;
+  var take_out_number = req.body.take_out_number;
+  var used = req.body.used;
+
+  console.log(id)
+  console.log(take_out_number)
+  console.log(used)
+
+  
+
+   pool.getConnection((err, connection) => {
+    if (err) throw err;
+    console.log("CONNECTION ESTABLISHED")
+
+    var sql = "UPDATE inventory SET used = '"+take_out_number+"' WHERE id='"+id+"'";
+
+    connection.query(sql, dataForm, (err, rows) => {
+
       connection.release();
 
       if (err) {
